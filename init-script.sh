c3634fd3e689f9db1908c7e13750bb8573777c4b
alirtasdelen <alitasdelen@ogrenci.karabuk.edu.tr>
2023-12-14 12:07:49 +0300

    init script optimized

--- init-script.sh
+++ init-script.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Fetch the IP address from Docker network inspect
-IP_ADDRESS=$(docker network inspect bridge -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}')
-
-# Update Prometheus configuration with obtained IP addresses
-sed -i "s/x.x.x.x/$IP_ADDRESS/g" prometheus.yml
-
 # Start Docker Compose services
 docker-compose up -d
